4226209557cde9a934a12846bda211277cae6842
Jagerziel <fromnytosg@gmail.com>
2023-09-30 19:31:41 -0400

    Updated test app with flatlist working component

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput, Alert, FlatList } from "react-native";
+
+const DATA = [
+  {
+    id: 1,
+    title: 'Meditation'
+  },
+  {
+    id: 2,
+    title: 'Coding'
+  },
+  {
+    id: 3,
+    title: 'Journeling'
+  }
+]
+
+const TodoItem = (props) => {
+  // console.log(props.item.title)
+  return (
+    <View>
+      <Text>{props.item.title}</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
-      <Text>This is my first project!</Text>
+      <Text>Testing Change!</Text>
       <StatusBar style="auto" />
+      <TextInput style={styles.input} />
+      <Button
+        title="start"
+        onPress={() => Alert.alert("simple button pressed")}
+      />
+      <FlatList
+        data={DATA}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => <TodoItem item={item} />}
+      />
     </View>
   );
 }
@@ -14,8 +48,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "gray",
   },
 });
